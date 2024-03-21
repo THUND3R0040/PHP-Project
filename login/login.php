97cd0bbf0e65fc5f237518049724553ff0b9b2af
Ahmed Gargouri <ahmed.gargouri@insat.ucar.tn>
2024-03-21 19:55:22 +0100

    first commit

---
+++ login/login.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css" />
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    
+    
+    
+    
+    
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    
+    
+    <meta charset="UTF-8">
+    <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" data-auto-replace-svg="nest"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <script src="https://use.fontawesome.com/a183c5deb5.js"></script>
+    <script src="https://kit.fontawesome.com/9d07734ae2.js" crossorigin="anonymous"></script>
+    <link href="login.css" rel="stylesheet">
+    
+    
+</head>
+
+
+
+
+
+    <body>
+    
+
+
+    
+    
+    <header>
+        <nav class="nav">
+            <div class="navbar">
+                <div class="logo"data-aos="fade-right">
+                    <img src="../mainPage/basketv3.png"  id="img">
+                </div>
+                <div class="links">
+                    <a href="../login/login.php"  id="a4"data-aos="fade-right">LOGIN</a>
+                    <a href="../product page/product.php" id="a1" data-aos="fade-right">PRODUCTS</a>
+                    <a href="#con" id="a2" data-aos="fade-right">CONTACT</a>
+
+                </div>
+            </div>
+    
+            <div class="mobile">
+                
+                <div class="logo2"data-aos="fade-right" >
+                    <img src="../basketv3.png"  id="img2">
+                </div>
+                <button id="bar"data-aos="fade-right" ><i class="fas fa-bars"
+                        style="color: rgb(255, 255, 255);font-size: 24px;"></i></button>
+    
+    
+            </div>
+        </nav>
+    
+    
+    </header>
+    
+    
+    
+    
+    
+    <section class="hero">
+    
+        <div class="form">
+            <div class="fr">
+                <h1>
+                    Welcome Back!
+                </h1>
+                <p>Did you know? When you create an account, you get access to cool stuff .</p>
+                <button class="sign-in"data-aos="zoom-in-up" data-aos-duration="2000">
+                    Sign In
+                </button>
+            </div>
+            <div class="fr2">
+                <h1>
+                    Create Account
+                </h1>
+                <div class="inputWithIcon"data-aos="zoom-in-left">
+                    <input type="text" placeholder="Name" class="inpt">
+                    <i class="fa fa-user fa-lg fa-fw" aria-hidden="true"></i>
+                </div>
+                <div class="inputWithIcon"data-aos="zoom-in-left">
+                    <input type="email" placeholder="Email" class="inpt">
+                    <i class="fa fa-envelope fa-lg fa-fw" aria-hidden="true"></i>
+                </div>
+                <div class="inputWithIcon"data-aos="zoom-in-left">
+                    <input type="password" placeholder="Password" class="inpt">
+                    <i class="fas fa-unlock-alt fa-lg fa-fw" aria-hidden="true"></i>
+                    
+                </div>
+                <button class="sign-up"data-aos="zoom-in-up" data-aos-duration="2000">Sign Up</button>
+            </div>
+        </div>
+    
+    
+    </section>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    <div class="phone">
+        <a href="../products/product.php" id="href1">Products</a>
+        <a href="#con" id="href2">Contact</a>
+        <a href="login.html" id="href4">Login</a>
+        <i class="far fa-window-close" id="close"></i>
+    </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    <section class="footer" id="con">
+    
+        <div class="half1">
+            <div class="f1">
+                <h1>COMPANY</h1>
+                <p>Who we are</p>
+                <p>Find Us Near You</p>
+                <p>Shop</p>
+            </div>
+            <div class="f1">
+                <h1>CUSTOMER CARE</h1>
+                <p>Gift Cards</p>
+                <p>Contact Us</p>
+                <p>Shipping</p>
+                <p>Returns</p>
+            </div>
+            <div class="f1">
+                <h1>THE DRY STUFF</h1>
+                <p>Privacy Policy</p>
+                <p>Terms & Conditions</p>
+                <p>Accessibility Statement</p>
+            </div>
+        </div>
+        <div class="acc">
+            <div id="accordion">
+                <h3>COMPANY</h3>
+                <div>
+                    <p>Who we are</p>
+                    <p>Find Us Near You</p>
+                    <p>Shop</p>
+                </div>
+                <h3>CUSTOMER CARE</h3>
+                <div>
+                    <p>Gift Cards</p>
+                    <p>Contact Us</p>
+                    <p>Shipping</p>
+                    <p>Returns</p>
+                </div>
+                <h3>THE DRY STUFF</h3>
+                <div>
+                    <p>Privacy Policy</p>
+                    <p>Terms & Conditions</p>
+                    <p>Accessibility Statement</p>
+                </div>
+    
+    
+    
+            </div>
+        </div>
+    
+        <div class="sep">
+        </div>
+    
+    
+        <div class="half2">
+            <div class="icons">
+                <button class="facebook sm" data-aos="zoom-in-right" data-aos-duration="500"><a
+                        href="https://www.facebook.com/ahmed.thunder.98" target="_blank"><i
+                            class="fab fa-facebook-f dd"></i></a></button>
+                <button class="twitter sm" data-aos="zoom-in-right" data-aos-duration="1000"><a
+                        href="https://twitter.com/THUNDER84630828" target="_blank"><i class="fab fa-twitter dd"></i></a></button>
+                <button class="linkedin sm" data-aos="zoom-in-right" data-aos-duration="1500"><a
+                        href="https://www.linkedin.com/in/ahmed-gargouri-2431b2204/" target="_blank"><i
+                            class="fab fa-linkedin-in dd "></i></a></button>
+                <button class="youtube sm" data-aos="zoom-in-right" data-aos-duration="2000"><a
+                        href="https://www.youtube.com/channel/UCFTewZU0FnvLtm5rSPGVurA" target="_blank"><i
+                            class="fab fa-youtube dd"></i></a></button>
+                <button class="instagram sm" data-aos="zoom-in-right" data-aos-duration="2500"><a href="" target="_blank"><i
+                            class="fab fa-instagram dd"></i></a></button>
+    
+            </div>
+            <div class="rights">all rights reserved © 2021</div>
+        </div>
+    
+    
+    
+    
+    
+    
+    </section>
+    
+    
+    
+    
+    <div class="grey">
+    
+    </div>
+    
+    <button class="btt"><i class="fas fa-chevron-up bt"></i></button>
+
+    
+        
+        <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+        <script>
+            AOS.init();
+        </script>
+        <script src="login.js">
+
+        </script>
+</body>
+</html>
